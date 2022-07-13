274e34bd47a0db3136ea3bf2312b24fad0e48a91
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-13 15:36:01 +0530

    Deleting utils from . and moving it to ./src
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- utils/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey: string | undefined =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
