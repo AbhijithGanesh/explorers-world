dc04b2972ccc6ae4d902154ca940d4f8e9f08fbf
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-19 02:04:02 +0530

    Update to configuration
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/components/auth/third-party.tsx
+++ src/components/auth/third-party.tsx
@@ -1,87 +1,11 @@
+import * as React from "react";
 import { FaGithub, FaGitlab, FaGoogle } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
-import { navigate } from "gatsby";
-import * as React from "react";
-import { supabase } from "../../utils/supabase";
+import Provider from "../../components/providers";
 
 type Props = {
   name: string;
   content: JSX.Element;
-};
-
-let Provider = ({ name, content }: Props): JSX.Element => {
-  switch (name) {
-    case "GitHub":
-      return (
-        <>
-          <button
-            onClick={async () => {
-              const { user, session, error } = await supabase.auth.signIn(
-                {
-                  provider: `github`,
-                },
-                { redirectTo: "./profiles/" }
-              );
-            }}
-          >
-            {content}
-          </button>
-        </>
-      );
-    case "GitLab":
-      return (
-        <>
-          <button
-            onClick={async () => {
-              const { user, session, error } = await supabase.auth.signIn(
-                {
-                  provider: `gitlab`,
-                },
-                { redirectTo: "./profiles/" }
-              );
-            }}
-          >
-            {content}
-          </button>
-        </>
-      );
-    case "Google":
-      return (
-        <>
-          <button
-            onClick={async () => {
-              const { user, session, error } = await supabase.auth.signIn(
-                {
-                  provider: `google`,
-                },
-                {
-                  redirectTo: "./profiles/",
-                }
-              );
-            }}
-          >
-            {content}
-          </button>
-        </>
-      );
-
-    case "Us":
-      return (
-        <>
-          <>
-            <button
-              onClick={async () => {
-                navigate("./signup");
-              }}
-            >
-              {content}
-            </button>
-          </>
-        </>
-      );
-    default:
-      return <></>;
-  }
 };
 
 type SocialAuth = {
