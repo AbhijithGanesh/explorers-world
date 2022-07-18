b3e3586369c7ac1db195f98095ddb12f70ce9fb8
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-18 22:46:46 +0530

    Third Party Provider patch
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
-import { navigate } from "gatsby";
 import * as React from "react";
 import IndexPage from "../components/content";
-import { supabase } from "../utils/supabase";
 
 const Index = (): JSX.Element => {
   return (
     <>
-      {/* {() => {
-        if (supabase.auth.session()?.user?.id) {
-          navigate("/profiles");
-        }
-      }} */}
       <IndexPage />
     </>
   );
