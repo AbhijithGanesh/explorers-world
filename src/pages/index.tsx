8def17c4c04afe1c421c726c1dd190e974870537
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-18 21:42:40 +0530

    Login patch
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ import { supabase } from "../utils/supabase";
 const Index = (): JSX.Element => {
   return (
     <>
-      {() => {
+      {/* {() => {
         if (supabase.auth.session()?.user?.id) {
           navigate("/profiles");
         }
-      }}
+      }} */}
       <IndexPage />
     </>
   );
