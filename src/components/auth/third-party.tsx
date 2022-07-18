f41d3501eb16655eeeaea6df71de1123b141d523
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-19 01:44:59 +0530

    Login TPA update
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/components/auth/third-party.tsx
+++ src/components/auth/third-party.tsx
@@ -1,7 +1,89 @@
-import * as React from "react";
 import { FaGithub, FaGitlab, FaGoogle } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
-import Provider from "../providers";
+import { navigate } from "gatsby";
+import * as React from "react";
+import { supabase } from "../../utils/supabase";
+
+type Props = {
+  name: string;
+  content: JSX.Element;
+};
+
+let Provider = ({ name, content }: Props): JSX.Element => {
+  switch (name) {
+    case "GitHub":
+      return (
+        <>
+          <button
+            onClick={async () => {
+              const { user, session, error } = await supabase.auth.signIn(
+                {
+                  provider: `github`,
+                },
+                { redirectTo: "./profiles/" }
+              );
+            }}
+          >
+            {content}
+          </button>
+        </>
+      );
+    case "GitLab":
+      return (
+        <>
+          <button
+            onClick={async () => {
+              const { user, session, error } = await supabase.auth.signIn(
+                {
+                  provider: `gitlab`,
+                },
+                { redirectTo: "./profiles/" }
+              );
+            }}
+          >
+            {content}
+          </button>
+        </>
+      );
+    case "Google":
+      return (
+        <>
+          <button
+            onClick={async () => {
+              const { user, session, error } = await supabase.auth.signIn(
+                {
+                  provider: `google`,
+                },
+                {
+                  redirectTo: "./profiles/",
+                }
+              );
+            }}
+          >
+            {content}
+          </button>
+        </>
+      );
+
+    case "Us":
+      return (
+        <>
+          <>
+            <button
+              onClick={async () => {
+                navigate("./signup");
+              }}
+            >
+              {content}
+            </button>
+          </>
+        </>
+      );
+    default:
+      return <></>;
+  }
+};
+export default Provider;
 
 type SocialAuth = {
   name: string;
