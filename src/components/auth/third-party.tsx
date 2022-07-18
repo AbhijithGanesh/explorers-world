c95783fe9bceb85c495c7749afdbd2270a1dc68c
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-19 01:53:53 +0530

    Resolving duplicate import
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/components/auth/third-party.tsx
+++ src/components/auth/third-party.tsx
@@ -2,7 +2,7 @@ import { FaGithub, FaGitlab, FaGoogle } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
 import { navigate } from "gatsby";
 import * as React from "react";
-import { supabase } from "../..-/utils/supabase";
+import { supabase } from "../../utils/supabase";
 
 type Props = {
   name: string;
