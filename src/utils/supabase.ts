40345f931cfa5e437e129c7a9a814d2cfd2aa433
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-19 11:31:10 +0530

    Resolution of Link
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,12 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { config as DotConfig } from "dotenv";
-
-DotConfig({
-  path: `.env.${process.env.NODE_ENV}`,
-});
+import { env } from "process";
 
 const supabaseUrl: string = "https://tqodfyadkocrkvniakxp.supabase.co";
-const supabaseAnonKey: string | undefined =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey: string | undefined = env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
