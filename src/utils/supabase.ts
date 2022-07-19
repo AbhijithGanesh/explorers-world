f746460ee56ad978abe9659a5750d0ce1f05b51f
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-19 12:20:30 +0530

    Resolution of supabase URL
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { env } from "process";
 
-const supabaseUrl: string = "https://tqodfyadkocrkvniakxp.supabase.co";
+const supabaseUrl: string = env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey: string | undefined = env.PRODUCTION_KEY;
 
 export const supabase = createClient(supabaseUrl!, supabaseKey!);
