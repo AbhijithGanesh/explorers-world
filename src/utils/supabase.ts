92e64e803782cdedd67b2ce37e21bcd6d5d4d519
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-19 11:15:02 +0530

    Supabase ANON Key patch
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
+import { config as DotConfig } from "dotenv";
 
-const supabaseUrl: string  = "https://tqodfyadkocrkvniakxp.supabase.co";
+DotConfig({
+  path: `../../.env.${process.env.NODE_ENV}`,
+});
+
+const supabaseUrl: string = "https://tqodfyadkocrkvniakxp.supabase.co";
 const supabaseAnonKey: string | undefined =
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
